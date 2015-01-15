b54e3b739383fcbdaec985b6a70a8c447eaefcd6
Scott <scott@sscotth.io>
2015-01-14 18:25:38 -0600

    get treehouse track urls

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 var Nightmare = require('nightmare');
+var camelCase = require('camel-case');
+var output = {};
 
 new Nightmare()
-  .goto('http://teamtreehouse.com')
-  .screenshot('screen.png')
+  .goto('http://teamtreehouse.com/tracks')
+  .evaluate(getTrackUrls, function(data){
+    setTrackUrls(data);
+  })
   .run(function (err, nightmare) {
     if (err) return console.log(err);
-    console.log('Done!');
+    console.log('Done!', output);
   });
+
+function getTrackUrls(){
+  return $('.track a.title').map(function(i, a){
+    return $(a).attr('href');
+  }).toArray();
+}
+
+function setTrackUrls(data){
+  return data
+    .map(function(url){
+      var key = trackUrlToKey(url);
+
+      output[key] = output[key] || {};
+      output[key].url = url;
+
+      return url;
+    });
+}
+
+function trackUrlToKey(url){
+  var hypenatedKey = url.split('/').pop();
+  return camelCase(hypenatedKey);
+}

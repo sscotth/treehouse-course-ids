d713d8af99499db8e96214b2a44bfb588ef9423c
Scott <scott@sscotth.io>
2015-02-05 19:11:55 -0600

    get Github commit history and post to Firebase

--- app.js
+++ app.js
@@ -1,96 +1,51 @@
+/* jshint node: true, devel: true */
+
 'use strict';
 
-var Nightmare = require('nightmare');
-var camelCase = require('camel-case');
-var _ = require('lodash');
+var Nightmare    = require('nightmare'),
+    _            = require('lodash'),
+    request      = require('request'),
+    FIREBASE_URL = 'https://yspuku7qvh9u4cr3.firebaseio.com/c8-students',
 
-var output = {};
+    app = new Nightmare();
 
-var app = new Nightmare()
-  .goto('http://teamtreehouse.com/tracks')
-  .evaluate(getTrackInfo, function(tracks){
-    _.each(tracks, function(track){
-      setTrackInfo(track);
-    });
-  })
-  .use(function(){
-    _.each(output, function(track, key){
-      var url = 'http://teamtreehouse.com' + track.url;
+request.get(FIREBASE_URL + '.json', function (err, res, body) {
+  _.forEach(JSON.parse(body), function (user, userId) {
+    var githubUsername = user.github.username || user.github,
+        url            = 'https://github.com/' + githubUsername,
+        history;
 
-      app
-        .use(function(){
-          console.log('Loading Url:', url);
-        })
-        .goto(url)
-        .evaluate(getTrackDetails, function(data){
-          output[key] = _.merge(output[key], data);
-        });
-    });
+    app
+      .goto(url)
+      .use(function () {
+        console.log('Loading Url:', url);
+      })
+      .evaluate(getUserNamesCommitHistory, function (data) {
+        history = data;
+      })
+      .use(function () {
+        console.log('Posting Data To Firebase');
+
+        request.put(FIREBASE_URL + '/' + userId + '/' + 'github' + '.json',
+          {
+            json: {
+              username: githubUsername,
+              history: history
+            }
+          }
+        );
+      });
   });
-
-app.run(function (err, nightmare) {
-  if (err) return console.log(err);
-
-  var camelizedOutput = allKeysToCamelCase(output);
-  console.log(JSON.stringify(camelizedOutput));
 });
 
-function getTrackDetails(){
-  var details = { sections: {} };
 
-  details.title = $('.hero-track h1').text().trim();
+app.run(function (err, nightmare) {
+  if (err) { return console.log(err); }
+});
 
-  $('.course-preview-modal').each(function(i, item){
-    var $item = $(item);
-    var hyphenated = $item.attr('id');
-
-    details.sections[hyphenated] = {};
-    details.sections[hyphenated].stageCount = +$item.find('h3').text().trim().split(' ')[0];
-    details.sections[hyphenated].title = $item.find('h2').text().trim();
-    details.sections[hyphenated].url = $item.find('.button-secondary').attr('href');
-  });
-
-  return details;
-}
-
-function getTrackInfo(){
-  return $('.track').map(function(i, track){
-    var info = {};
-    var $track = $(track);
-    var classList = $track.attr('class').split(' ');
-
-    info.url = $track.find('a.title').attr('href');
-    info.area = _.difference(classList, ['track', 'card'])[0];
-    info.name = $track.find('a.title h3').text();
-
-    return info;
+function getUserNamesCommitHistory() {
+  /* jshint jquery: true */
+  return $('[data-count]').map(function (i, dayNode) {
+    return $(dayNode).data('count');
   }).toArray();
-}
-
-function setTrackInfo(trackInfo){
-  var key = trackUrlToKey(trackInfo.url);
-
-  output[key] = trackInfo;
-
-  return trackInfo;
-}
-
-function trackUrlToKey(url){
-  return url.split('/').pop();
-}
-
-function allKeysToCamelCase(obj){
-  var output = {};
-
-  // Recursively changes object keys to use camelCase
-  // similar to the solution at: http://stackoverflow.com/a/10196772
-  for (var i in obj) {
-    if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
-      output[camelCase(i)] = allKeysToCamelCase(obj[i]);
-    } else {
-      output[camelCase(i)] = obj[i];
-    }
-  }
-
-  return output;
 }
